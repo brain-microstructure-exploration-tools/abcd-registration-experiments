35f9e77c41bd22122ad0e5af22ee120e0d727b30
Ebrahim Ebrahim <ebrahim.mt@gmail.com>
2022-09-21 00:49:46 -0400

    update downloader and extractor for new sample
    This also updates downloader command for the newer version of
    nda-downloader, which wisely removed the --pasword argument

--- 02.0_abcd_download.sh
+++ 02.0_abcd_download.sh
@@ -3,4 +3,5 @@
 # This needs the NDA download tool:
 # `pip install nda-tools`. Then `downloadcmd --help` to see usage.
 
-downloadcmd -t ~/abcd-registration-experiments/abcd-sample/sample_derived_files.txt -d ~/data/abcd/Package_1200530/ -u ebrahim -p '<put nda pw here>' -dp 1200530
+# Fill in your own username, and when prompted provide your own password
+downloadcmd -t ~/abcd-registration-experiments/01.1_abcd_sample2/sample_all_derived_files.txt -d ~/data/abcd/Package_1200530/ -u ebrahim -dp 1200530
--- 03.0_extract_all_images.sh
+++ 03.0_extract_all_images.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-
-ARCHIVES_PATH="/home/ebrahim/data/abcd/Package_1200530/fmriresults01/abcd-mproc-release4"
-DESTINATION_PATH="/home/ebrahim/data/abcd/DMRI_extracted"
-
 set -e
 
-for A in $ARCHIVES_PATH/*.tgz; do
+# The base directory to which the archive files were downloaded
+ARCHIVES_PATH="/home/ebrahim/data/abcd/Package_1200530/fmriresults01/abcd-mproc-release4"
+
+if [[ $# -ne 2 ]]; then
+  echo "Usage: ./03.0_extract_all_images.sh  <DERIVED_FILE_LIST>  <DESTINATION_PATH>"
+  echo "  where DERIVED_FILE_LIST is the file containing the list of S3 links, the same one you gave to NDA download manager,"
+  echo "  and where DESTINATION_PATH is the directory to which you want to dump the extracted images."
+  exit
+fi
+
+# The file containing the list of S3 links, the same one we gave to NDA download manager
+DERIVED_FILE_LIST=$1
+
+# The directory in which we want to put the extracted images
+DESTINATION_PATH=$2
+
+
+sed 's/.*\///' $DERIVED_FILE_LIST | while read ARCHIVE_NAME ; do
+  A=$ARCHIVES_PATH/$ARCHIVE_NAME
   [ -f "$A" ] || break
   D=$DESTINATION_PATH/$(basename $A .tgz)
   if [ -d $D ]
