9de0094d392ba7eeb80ae0d85e56f1a6a9b985bb
James Fishbaugh <james.fishbaugh@kitware.com>
2024-04-17 20:57:45 -0400

    fix point indexing error

--- evaluation_framework/compute_tract_distance.py
+++ evaluation_framework/compute_tract_distance.py
@@ -2,55 +2,28 @@ import argparse
 import vtk
 import numpy as np
 
-import time
+import fiber_measures
+import fiber_tract_io
 
-# This will be the mean closest fiber distance
-# Gerig et al. (2004) as ‘‘the mean of closest distances’’
-# It is defined as ‘‘the mean of the closest distance for every point of two fibers’’
-# 
-# Go through all points of fiber fm and find the point on fiber ft that is closest
-# Measure this distance and record it
-# Compute the average of all these distances
-def fiber_distance(points_m, points_t):
-  
-  # Reshape for vectorized calculation
-  points_m = points_m.reshape((len(points_m), 1, 3))
-  points_t = points_t.reshape((1, len(points_t), 3))
+def main(fiber_tract_M_path, fiber_tract_T_path, percent_sample_fibers):
+ 
+  # Read tck fiber bundles
+  fiber_tract_M_header, fiber_tract_M_points_tck, fiber_tract_M_line_starts, fiber_tract_M_line_ends = fiber_tract_io.read_tck(fiber_tract_M_path)
+  fiber_tract_T_header, fiber_tract_T_points_tck, fiber_tract_T_line_starts, fiber_tract_T_line_ends = fiber_tract_io.read_tck(fiber_tract_T_path)
 
-  # fm - ft (num_points_f, num_points_t, 3) -- for an element i,j this is the coordinate distance between fm_i and ft_j
-  fm_minus_ft = points_m - points_t
-  squared = fm_minus_ft**2
-  
-  # We should sum 3rd dim (x,y,z) coords and take the sqrt
-  sum_of_squares = np.sum(squared, axis=2)
-  distances_fm_ft = np.sqrt(sum_of_squares)
+  # Fiber bundle M as polydata
+  fiber_bundle_M = fiber_tract_io.get_vtk_polydata(fiber_tract_M_points_tck, fiber_tract_M_line_starts, fiber_tract_M_line_ends)
 
-  # Now we the need the minimum value of each row which should represent for point fm_i the closest point on ft
-  mins_fm = np.min(distances_fm_ft, axis=1)
-
-  return np.mean(mins_fm)
-
-def main(fiber_bundle_M_path, fiber_bundle_T_path, percent_sample_fibers, percent_sample_points):
-
-  # Read fiber bundle M as polydata
-  reader_M = vtk.vtkPolyDataReader()
-  reader_M.SetFileName(fiber_bundle_M_path)
-  reader_M.Update()
-  fiber_bundle_M = reader_M.GetOutput()
-
-  # Read fiber bundle T as polydata
-  reader_T = vtk.vtkPolyDataReader()
-  reader_T.SetFileName(fiber_bundle_T_path)
-  reader_T.Update()
-  fiber_bundle_T = reader_T.GetOutput()
+  # Fiber bundle T as polydata
+  fiber_bundle_T = fiber_tract_io.get_vtk_polydata(fiber_tract_T_points_tck, fiber_tract_T_line_starts, fiber_tract_T_line_ends)
 
   # These are all the points which make up the fibers (streamlines) of fiber bundle M
-  fiber_bundle_M_points = fiber_bundle_M.GetPoints()
+  fiber_tract_M_points = fiber_bundle_M.GetPoints()
   fiber_bundle_M.GetLines().InitTraversal()
   idListM = vtk.vtkIdList()
 
   # These are all the points which make up fiber bundle T
-  fiber_bundle_T_points = fiber_bundle_T.GetPoints()
+  fiber_tract_T_points = fiber_bundle_T.GetPoints()
 
   iter_counter_M = -1  
   index_counter_M = 0
@@ -59,7 +32,7 @@ def main(fiber_bundle_M_path, fiber_bundle_T_path, percent_sample_fibers, percen
   num_fibers_M = int(np.ceil(fiber_bundle_M.GetNumberOfLines() * percent_sample_fibers))
   num_fibers_T = int(np.ceil(fiber_bundle_T.GetNumberOfLines() * percent_sample_fibers))
 
-  #np.random.seed(0)
+  np.random.seed(0)
   range_fibers_M = np.arange(0, fiber_bundle_M.GetNumberOfLines())
   sampling_fibers_M = np.random.choice(range_fibers_M, size=num_fibers_M, replace=False)
 
@@ -79,22 +52,16 @@ def main(fiber_bundle_M_path, fiber_bundle_T_path, percent_sample_fibers, percen
     if (iter_counter_M not in sampling_fibers_M):
       continue
     
-    num_points_fm = int(np.ceil(idListM.GetNumberOfIds()*percent_sample_points))
-    range_points_fm = np.arange(0, idListM.GetNumberOfIds())
-    sampling_points_fm = np.random.choice(range_points_fm, size=num_points_fm, replace=False)
-    
+    num_points_fm = idListM.GetNumberOfIds()
     fiber_fm_points = np.zeros((num_points_fm, 3))
     point_counter_fm = 0
 
     # Go through all points of fiber fm
     for pointId_fm_i in range(idListM.GetNumberOfIds()):
-
-      if (point_counter_fm not in sampling_points_fm):
-        continue
-
-      fiber_fm_points[point_counter_fm, :] = fiber_bundle_M_points.GetPoint(pointId_fm_i)
-      point_counter_fm+=1
       
+      fiber_fm_points[point_counter_fm, :] = fiber_tract_M_points.GetPoint(idListM.GetId(pointId_fm_i))
+      point_counter_fm+=1
+
     fiber_bundle_T.GetLines().InitTraversal()
     idListT = vtk.vtkIdList()   
 
@@ -109,25 +76,20 @@ def main(fiber_bundle_M_path, fiber_bundle_T_path, percent_sample_fibers, percen
       if (iter_counter_T not in sampling_fibers_T):
         continue
 
-      num_points_ft = int(np.ceil(idListT.GetNumberOfIds()*percent_sample_points))
-      range_points_ft = np.arange(0, idListT.GetNumberOfIds())
-      sampling_points_ft = np.random.choice(range_points_ft, size=num_points_ft, replace=False)
+      num_points_ft = idListT.GetNumberOfIds()
       
       fiber_ft_points = np.zeros((num_points_ft, 3))
       point_counter_ft = 0
       
       # Go through all points of fiber ft
       for pointId_ft_j in range(idListT.GetNumberOfIds()):
-        
-        if (point_counter_ft not in sampling_points_ft):
-          continue 
           
-        fiber_ft_points[point_counter_ft, :] = fiber_bundle_T_points.GetPoint(pointId_ft_j)
+        fiber_ft_points[point_counter_ft, :] = fiber_tract_T_points.GetPoint(idListT.GetId(pointId_ft_j))
         point_counter_ft+=1
-        
+
       # Now compute d(fm, ft) and d(ft, fm) store it
-      pairwise_distances_M_and_T[index_counter_M, index_counter_T] = fiber_distance(fiber_fm_points, fiber_ft_points)
-      pairwise_distances_T_and_M[index_counter_T, index_counter_M] = fiber_distance(fiber_ft_points, fiber_fm_points)
+      pairwise_distances_M_and_T[index_counter_M, index_counter_T] = fiber_measures.mean_closest_fiber_distance(fiber_fm_points, fiber_ft_points)
+      pairwise_distances_T_and_M[index_counter_T, index_counter_M] = fiber_measures.mean_closest_fiber_distance(fiber_ft_points, fiber_fm_points)
 
       index_counter_T+=1
 
@@ -143,30 +105,21 @@ if __name__=="__main__":
 
   #t = time.process_time()
 
-  parser = argparse.ArgumentParser(description='Measures the distance between two fiber bundles (see Zhang 2006 MEDIA eqn 10)')
+  parser = argparse.ArgumentParser(description='Measures the distance between two fiber tracts in tck format (see Zhang 2006 MEDIA eqn 10)')
 
   parser.add_argument('--percent_sample_fibers', default=0.5, type=float, help='Randomly sample a percentage of fiber streamlines')
-  parser.add_argument('--percent_sample_points', default=1.0, type=float, help='Randomly sample a percentage of points per streamline')
 
-  parser.add_argument('bundle1', type=str, help='path to a fiber bundle')
-  parser.add_argument('bundle2', type=str, help='path to a fiber bundle')
+  parser.add_argument('tract1', type=str, help='path to a tck fiber bundle')
+  parser.add_argument('tract2', type=str, help='path to a tck fiber bundle')
 
   args = parser.parse_args()
 
-  fiber_bundle_M_path = args.bundle1
-  fiber_bundle_T_path = args.bundle2
+  fiber_tract_M_path = args.tract1
+  fiber_tract_T_path = args.tract2
   percent_sample_fibers = args.percent_sample_fibers
-  percent_sample_points = args.percent_sample_points
 
   # Avoid bad inputs
   if (percent_sample_fibers <= 0.0) or (percent_sample_fibers > 1.0):
     raise Exception("--percent_sample_fibers must be in the range (0.0, 1.0]")
   
-  if (percent_sample_points <= 0.0) or (percent_sample_points > 1.0):
-    raise Exception("--percent_sample_points must be in the range (0.0, 1.0]")
-
-  main(fiber_bundle_M_path, fiber_bundle_T_path, percent_sample_fibers, percent_sample_points) 
-
-  #elapsed_time = time.process_time() - t
-  #time_str = 'Completed in %0.2f seconds' %(elapsed_time)
-  #print(elapsed_time)
+  main(fiber_tract_M_path, fiber_tract_T_path, percent_sample_fibers) 
