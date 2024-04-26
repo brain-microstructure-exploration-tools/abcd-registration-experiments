e75afca67eb79ff2f31ede8b3c4d3e4b33e511ae
James Fishbaugh <james.fishbaugh@kitware.com>
2024-04-26 13:14:42 -0400

    use path objects

--- evaluation_framework/transformation_utils.py
+++ evaluation_framework/transformation_utils.py
@@ -14,28 +14,32 @@ def convert_ants_transform_to_mrtrix_transform(target_image, ants_transform):
         identity_warp = '%s/identity_warp[].nii.gz' %(str(temp_dir))
 
         # Create identity warp
-        subprocess.run(['warpinit', target_image, identity_warp, '-force'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        subprocess.run(['warpinit', str(target_image), identity_warp, '-force'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         # Transform the idenity warp using ants
-        ants_target_im = ants.image_read(target_image)
+        ants_target_im = ants.image_read(str(target_image))
 
         image_dim = len(ants_target_im.numpy().shape)
         
         # Transform each dimensions
         for d in range(0, image_dim):
 
-            cur_identity_warp = '%s/identity_warp%d.nii.gz' %(temp_dir, d)
+            cur_identity_warp = '%s/identity_warp%d.nii.gz' %(str(temp_dir), d)
 
             cur_ants_source_im = ants.image_read(cur_identity_warp)
             
             cur_warped_image = ants.apply_transforms(fixed=ants_target_im, moving=cur_ants_source_im, transformlist=ants_transform, defaultvalue=2147483647)
 
-            out_image_filename = '%s/mrtrix_warp%d.nii.gz' %(temp_dir, d)
+            out_image_filename = '%s/mrtrix_warp%d.nii.gz' %(str(temp_dir), d)
             ants.image_write(cur_warped_image, out_image_filename)
 
-        corrected_warp = '%s/corrected_warp.nii.gz' %(temp_dir)
-        warp_filename = '%s/mrtrix_warp[].nii.gz' %(temp_dir)
+        corrected_warp = '%s/corrected_warp.nii.gz' %(str(temp_dir))
+        warp_filename = '%s/mrtrix_warp[].nii.gz' %(str(temp_dir))
         subprocess.run(['warpcorrect', warp_filename, corrected_warp, '-marker', '2147483647', '-force'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
         array_im = nib.load(corrected_warp)
         return nib.Nifti1Image(array_im.get_fdata(), array_im.affine)
+    
+def warp_fiber_tract(in_fiber_tract_path, mrtrix_warp_path, out_fiber_tract_path):
+
+    subprocess.run(['tcktransform', in_fiber_tract_path, mrtrix_warp_path, out_fiber_tract_path, '-force'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
